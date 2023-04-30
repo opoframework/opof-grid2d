8a0fb866f1e313db9ac81ae3d8f15dddf4e3337b
LeeYiyuan <leeyiyuan@hotmail.com>
2023-04-30 11:26:44 -0500

    Reduce python versions

--- setup.py
+++ setup.py
@@ -35,7 +35,6 @@ setup(
         "License :: OSI Approved :: BSD License",
         "Programming Language :: Python :: 3.9",
         "Programming Language :: Python :: 3.10",
-        "Programming Language :: Python :: 3.11",
     ],
     python_requires=">=3.9",
     install_requires=[
